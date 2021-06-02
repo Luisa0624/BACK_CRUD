96a261ad88dced575880658a6f49ca6841e7d64c
Luisa Gil <luisamariagilgarcia24@gmail.com>
2021-06-02 01:15:39 -0500

    Se crea back con Springboot

---
+++ src/main/java/com/springboot/kata_crud/Todo.java
@@ -0,0 +1,39 @@
+package com.springboot.kata_crud;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Todo {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String name;
+    private Boolean isComplete;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Boolean getComplete() {
+        return isComplete;
+    }
+
+    public void setComplete(Boolean complete) {
+        isComplete = complete;
+    }
+}
---
+++ src/main/java/com/springboot/kata_crud/TodoRepository.java
@@ -0,0 +1,7 @@
+package com.springboot.kata_crud;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface TodoRepository extends CrudRepository<Todo, Long> {
+
+}
